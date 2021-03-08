9fde72a9636f4d8938686dcd96757d8638989d28
Guilherme Pimenta <guialvespimenta27@gmail.com>
2021-03-08 13:11:38 -0300

    projeto mais otimizado

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import App from './components/Screens/signin';
+import Home from './components/Screens/home';
+import Search from './components/Screens/search';
+import { BrowserRouter,Route,Switch } from "react-router-dom";
+import {AuthProvider} from './provider/auth'
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <Switch>
+        <AuthProvider>
+          <Route exact path="/" component={App}/>
+          <Route exact path="/home" component={Home}/>
+          <Route exact path="/search" component={Search}/>
+          </AuthProvider>
+        </Switch>
+      </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
